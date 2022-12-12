51038c9ecc9f63058f06046ae6865eb109267efd
Andrii Varenko <andrijvarenko@gmail.com>
2022-12-12 20:39:11 +0200

    Edit "result"

--- src/main/java/converter/ConverterWrapper.java
+++ src/main/java/converter/ConverterWrapper.java
@@ -3,12 +3,7 @@ package converter;
 import java.math.BigDecimal;
 
 public class ConverterWrapper implements ConstantsConverter {
-	
-	private final static String TEMPLATE_CONSTANT = "%s %s = %s %s";
-	private final static String LIST_ARGUMENTS = ", %s, %s, %s, %s";
-	private final static char SPACE = ' ';
-	private final static char EQUALS = '=';
-	
+
 	private final Converter converter = new Converter (); 
 	
 	public String convert (BigDecimal amount, String currency, BigDecimal fee)
@@ -21,38 +16,19 @@ public class ConverterWrapper implements ConstantsConverter {
 		if (currency == null || currency.isEmpty() ||
 				(!currency.equalsIgnoreCase(USD) && !currency.equalsIgnoreCase(EUR))) {
 			throw new CurrencyException ();
-		}	
-		
-		StringBuilder result = new StringBuilder ()
-				//.append(TEMPLATE_CONSTANT)
-				//.append(LIST_ARGUMENTS)
-				.append(amount);
+		}
+
+		String result = null;
 
 		if (currency.equalsIgnoreCase (USD)) {
-			result
-					.append(SPACE)
-					.append(USD)
-					.append(SPACE)
-					.append(EQUALS)
-					.append(SPACE)
-					.append(converter.conversionUSDtoEUR (amount, fee))
-					.append(SPACE)
-					.append(EUR);
+			result = String.format("%s %s = %s %s", amount, USD, converter.conversionUSDtoEUR(amount, fee), EUR);
 		}
 		
 		if (currency.equalsIgnoreCase (EUR)) {
-			result
-					.append(SPACE)
-					.append(EUR)
-					.append(SPACE)
-					.append(EQUALS)
-					.append(SPACE)
-					.append(converter.conversionEURtoUSD (amount, fee))
-					.append(SPACE)
-					.append(USD);
+			result = String.format("%s %s = %s %s", amount, EUR, converter.conversionUSDtoEUR(amount, fee), USD);
 		}
 		
-		return result.toString();
+		return result;
 	}
 
 	private boolean isNotValid (BigDecimal amount, BigDecimal fee) {
